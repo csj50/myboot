ff40ed5a9e7084a19fb671d59cdd97801842b697
sjcui <287229112@qq.com>
2022-01-28 17:26:04 +0800

    配置druid监控页面

--- myboot/src/main/java/com/example/config/WebMvcConfig.java
+++ myboot/src/main/java/com/example/config/WebMvcConfig.java
@@ -9,18 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * 拦截器配置类
+ * 
  * @author User
  *
  */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-	//在此处，将拦截器注册为一个Bean，才能使用@Autowired注解注入对象
-    @Bean
-    public RateLimitInterceptor rateLimitInterceptor() {
-        return new RateLimitInterceptor();
-    }
-    
+	// 在此处，将拦截器注册为一个Bean，才能使用@Autowired注解注入对象
+	@Bean
+	public RateLimitInterceptor rateLimitInterceptor() {
+		return new RateLimitInterceptor();
+	}
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		// 注册RateLimitInterceptor拦截器
@@ -28,10 +29,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		InterceptorRegistration registration = registry.addInterceptor(this.rateLimitInterceptor());
 		// 所有路径都被拦截
 		registration.addPathPatterns("/**")
-		//排除拦截swagger页面
-		.excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**","doc.html","/error")
-		//排除拦截webapp目录下的jsp和html页面
-		.excludePathPatterns("/*.jsp", "/*.html");
+				// 排除拦截swagger页面
+				.excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**",
+						"doc.html", "/error")
+				// 排除拦截webapp目录下的jsp和html页面
+				.excludePathPatterns("/*.jsp", "/*.html")
+				// 排除拦截druid
+				.excludePathPatterns("/druid/**");
 		WebMvcConfigurer.super.addInterceptors(registry);
 	}
 
@@ -39,8 +43,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("swagger-ui.html", "doc.html")
 				.addResourceLocations("classpath:/META-INF/resources/");
-		registry.addResourceHandler("/webjars/**")
-				.addResourceLocations("classpath:/META-INF/resources/webjars/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 
 	}
 
